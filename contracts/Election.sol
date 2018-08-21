dc9da56d8362bf7d8dbf833e7c35dd8e08dfb5c8
Vick Kumar <vick.kumar44@gmail.com>
2018-08-21 15:54:02 -0400

    reset migration

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-  	// Store candidate
-  	// Read candidate
-    string public candidate;
+  	// Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Store candidates
+    // Fetch candidates
+    mapping(uint => Candidate) public candidates;
+    // Store candidates count
+    uint public candidatesCount;
   	// Constructor
     constructor() public{
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
