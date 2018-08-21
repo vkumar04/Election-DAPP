aa6e0dd8c710ffbd56d7a18a10b4c322ed481ef3
Vick Kumar <vick.kumar44@gmail.com>
2018-08-21 16:48:44 -0400

    setup mocha test

---
+++ test/election.js
@@ -0,0 +1,27 @@
+const Election = artifacts.require("./Election.sol");
+
+contract("Election", (accounts) => {
+  it("initializes with two canidates", () => {
+    return Election.deployed().then(instance => {
+      return instance.candidatesCount();
+    }).then((count) => {
+      assert.equal(count, 2)
+    });
+  });
+
+  it("it initializes the candidates with the correct values", () => {
+    return Election.deployed().then((instance) => {
+      electionInstance = instance;
+      return electionInstance.candidates(1);
+    }).then(candidate => {
+      assert.equal(candidate[0], 1, "contains the correct id");
+      assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
+      return electionInstance.candidates(2);
+    }).then(candidate => {
+      assert.equal(candidate[0], 2, "contains the correct id");
+      assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
+    });
+  });
+});
